4deb15f55ab490b8174074c96c1bd450315b48bf
Krittin Kuncharoen <5506021610039@fitm.kmutnb.ac.th>
2016-04-24 14:17:42 +0700

    5 commit

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require('express')
 var app = express()
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
@@ -13,6 +16,20 @@ app.get('/webhook/', function (req, res) {
 })
 
 app.set('port', (process.env.PORT || 3000))
+
+app.post('/webhook/', function (req, res) {
+  var messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    var event = req.body.entry[0].messaging[i];
+    var sender = event.sender.id;
+    if (event.message && event.message.text) {
+      var text = event.message.text;
+      // Handle a text message from this sender
+    }
+    console.log(text);
+  }
+  res.sendStatus(200);
+});
 
 app.listen(app.get('port'), function () {
   console.log('Example aoo listening on port ' + app.get('port') + ' !')
