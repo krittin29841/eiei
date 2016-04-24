59cdb6263e1e367dc15e8f032075e8b411af61bb
Krittin Kuncharoen <5506021610039@fitm.kmutnb.ac.th>
2016-04-24 14:46:22 +0700

    5 commit

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.post('/webhook/', function (req, res) {
     if (event.message && event.message.text) {
       var text = event.message.text;
       // Handle a text message from this sender
-      if(text == "กนก")
+      if(text == "ติน")
       	sendTextMessage(sender, "กนก")
       else
       	sendTextMessage(sender, "หวัดดีครับ")
